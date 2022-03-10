1ec53bb16c9b6340b2f21b5b0737fe1b5a9ae4d3
Bárbara <barbarap@tutanota.com>
2022-03-10 11:21:04 +0100

    integration test

--- src/test/java/de/neuefische/dockermongodbdemo/DockerMongodbDemoApplicationTests.java
+++ src/test/java/de/neuefische/dockermongodbdemo/DockerMongodbDemoApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -21,14 +22,32 @@ class DockerMongodbDemoApplicationTests {
 		Question question = new Question();
 		question.setQuestion("is forró tipically played with triangolo?");
 		question.setAnswer("yes");
-		question.setId("2");
 
 		ResponseEntity<Question> questionResponse = restTemplate.postForEntity("/api/questions", question, Question.class);
 		assertThat(questionResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
 		assertThat(questionResponse.getBody().getQuestion()).isEqualTo("is forró tipically played with triangolo?");
-		assertThat(questionResponse.getBody().getQuestion()).isEqualTo("x");
+		assertThat(questionResponse.getBody().getAnswer()).isEqualTo("yes");
+		assertThat(questionResponse.getBody().getId()).isNotNull();
 	}
 
+    @Test
+	void contextDelete (){
+		Question question = new Question();
+		question.setQuestion("is forró tipically played with triangolo?");
+		question.setAnswer("yes");
 
+
+		ResponseEntity<Question> questionResponse = restTemplate.postForEntity("/api/questions", question, Question.class);
+		assertThat(questionResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(questionResponse.getBody().getQuestion()).isEqualTo("is forró tipically played with triangolo?");
+		assertThat(questionResponse.getBody().getAnswer()).isEqualTo("yes");
+		assertThat(questionResponse.getBody().getId()).isNotNull();
+
+		ResponseEntity<Void> questionDelete =  restTemplate.exchange("/api/questions/" + questionResponse.getBody().getId(), HttpMethod.DELETE,null,Void.class );
+		assertThat(questionDelete.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		ResponseEntity<Question> questionGet = restTemplate.getForEntity ("/api/questions/" + questionResponse.getBody().getId(), Question.class);
+		assertThat(questionGet.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
 
 }
